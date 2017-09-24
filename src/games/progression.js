0b708f139852cb5eb2ef4355435117fddea62f68
ayubov <alvi.ayubov@yandex.ru>
2017-09-24 21:57:08 +0300

    add progression game

---
+++ src/games/progression.js
@@ -0,0 +1,22 @@
+import playGame from '..';
+import getRandomNumber from '../utils';
+
+const makeProgression = (current, step) => (current.length === 10 ?
+  current : makeProgression([...current, current.slice(-1)[0] + step], step));
+const hideElement = (arr, targetElement) => arr
+  .reduce((acc, e, index) => (index !== targetElement ? acc.concat(e) : acc.concat('..')), [])
+  .join(' ');
+const task = 'What number is missing in this progression?';
+
+export default () => {
+  const gameData = () => {
+    const startOfProgression = [getRandomNumber(100)];
+    const step = getRandomNumber(10);
+    const invisibleNumberIndex = getRandomNumber(10);
+    const fullProgression = makeProgression(startOfProgression, step);
+    const question = `${hideElement(fullProgression, invisibleNumberIndex)}`;
+    const correctAnswer = String(fullProgression[invisibleNumberIndex]);
+    return { question, correctAnswer };
+  };
+  return playGame(gameData, task);
+};
