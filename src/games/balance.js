5c1f2acb4765214ee1b87cf8c66f4cf01d00b0d6
ayubov <alvi.ayubov@yandex.ru>
2017-09-24 20:38:30 +0300

    fix mutable balance

--- src/games/balance.js
+++ src/games/balance.js
@@ -1,21 +1,21 @@
 import playGame from '..';
 import getRandomNumber from '../utils';
 
-export default () => {
-  const balance = (number) => {
-    const arrOfDigits = String(number).split('').map(e => Number(e));
-    const getBalance = (arr) => {
-      const sortedArr = arr.slice().sort((a, b) => a - b);
-      if (sortedArr[sortedArr.length - 1] - sortedArr[0] < 2) {
-        return sortedArr.join('');
-      }
-      sortedArr[0] += 1;
-      sortedArr[sortedArr.length - 1] -= 1;
-      return getBalance(sortedArr);
-    };
-    return getBalance(arrOfDigits);
+const balance = (number) => {
+  const arrOfDigits = String(number).split('').map(e => Number(e));
+  const getBalance = (arr) => {
+    const sortedArr = arr.slice().sort((a, b) => a - b);
+    if (sortedArr[sortedArr.length - 1] - sortedArr[0] < 2) {
+      return sortedArr.join('');
+    }
+    const newArr = [sortedArr[0] + 1, ...sortedArr.slice(1, -1), sortedArr.slice(-1)[0] - 1];
+    return getBalance(newArr);
   };
-  const task = 'Balance the given number.';
+  return getBalance(arrOfDigits);
+};
+const task = 'Balance the given number.';
+
+export default () => {
   const gameData = () => {
     const num = getRandomNumber(10000);
     const question = `${num}`;
