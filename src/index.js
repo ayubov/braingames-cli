4fd18e5872c39f00654b43dba1ca51b22b848ede
ayubov <alvi.ayubov@yandex.ru>
2017-09-25 00:07:04 +0300

    delete useless line

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import readlineSync from 'readline-sync';
 
 export default (gameDetails, task) => {
   console.log('Welcome to the Brain Games!');
-  const taskDescription = task;
-  console.log(taskDescription);
+  console.log(task);
+
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
+
   const maxRounds = 3;
   for (let roundCounter = 0; roundCounter < maxRounds; roundCounter += 1) {
     const { correctAnswer, question } = gameDetails();
