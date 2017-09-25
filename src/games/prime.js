8acc7cc7e9ddb82248e39202df9a3065eb0a22aa
ayubov <alvi.ayubov@yandex.ru>
2017-09-25 13:52:20 +0300

    make isPrime clear function

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,19 +1,23 @@
 import playGame from '..';
 import getRandomNumber from '../utils';
 
-const isPrime = (num, divisor) => {
-  if (divisor === 1) {
-    return 'yes';
-  }
-  return num % divisor === 0 ? 'no' : isPrime(num, divisor - 1);
+const isPrime = (num) => {
+  const iter = (number, divisor) => {
+    if (divisor === 1) {
+      return true;
+    }
+    return number % divisor === 0 ? false : iter(number, divisor - 1);
+  };
+  return iter(num, num - 1);
 };
+
 const task = 'Is number prime?';
 
 export default () => {
   const gameData = () => {
     const number = getRandomNumber(100);
     const question = `${number}`;
-    const correctAnswer = isPrime(number, number - 1);
+    const correctAnswer = isPrime(number) ? 'yes' : 'no';
     return { question, correctAnswer };
   };
   return playGame(gameData, task);
